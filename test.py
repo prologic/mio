bb2cb308a99e2891ee0779f45eede693c101f4fd
millsjam@WDKP63100.org.local <millsjam@WDKP63100.org.local>
2012-04-13 08:38:15 +1000

    ...

--- test.py
+++ test.py
@@ -6,32 +6,31 @@ from mio.object import Object
 
 class Method(object):
 
-    def __init__(self, name=None):
+    def __init__(self, f, name=None):
         super(Method, self).__init__()
 
+        self.f = f
         self.name = name
 
-    def __call__(self, f):
-        def wrapper(sender, receiver, context, *args):
-            self.receiver = receiver
-            self.context = context
-
-            return f(sender, *args)
-
-        wrapper.method = True
-
-        return update_wrapper(wrapper, f)
+    def __call__(*args):
+        print(repr(args))
+        #return self.f(*args)
 
     def __repr__(self):
         return "method(...)"
 
-method = Method
+def method(name=None):
+    def wrapper(f):
+        f.method = True
+        return f
+
+    return wrapper
 
 class Foo(Object):
 
     @method()
     def foo(self, a, b):
-        print(self.receiver, self.context)
+        #print(self.receiver, self.context)
         print(a, b)
 
 foo = Foo()
