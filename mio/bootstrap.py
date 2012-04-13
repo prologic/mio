cc384f8db3c4db3ebbf6157ea6d2ce821779e7d2
prologic <none@none>
2012-04-14 02:03:03 +1000

    Removed Message from Lobby

--- mio/bootstrap.py
+++ mio/bootstrap.py
@@ -12,7 +12,6 @@ Lobby["Object"] = Object
 Lobby["Number"] = Number(0, Object)
 
 Lobby["List"] = Object.clone([])
-Lobby["Message"] = Object.clone()
 Lobby["String"] = Object.clone("")
 Lobby["None"] = Object.clone(None)
 Lobby["True"] = Object.clone(True)
