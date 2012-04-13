ce91ba71fa9ddc2f4ea7a5036bfbff120c0c4880
prologic <none@none>
2012-04-14 00:48:45 +1000

    Implemented __int__ and __float__ functions/methods on Number

--- mio/core/number.py
+++ mio/core/number.py
@@ -2,6 +2,8 @@ from mio.object import method, Object
 
 class Number(Object):
 
+    # General Arithmetic
+
     @method()
     def __add__(self, other):
         return self.clone(self.value.__add__(other.value))
@@ -17,3 +19,13 @@ class Number(Object):
     @method()
     def __div__(self, other):
         return self.clone(self.value.__div__(other.value))
+
+    # Type Conversion
+
+    @method()
+    def __int__(self):
+        return self.clone(self.value.__int__())
+
+    @method()
+    def __float__(self):
+        return self.clone(self.value.__float__())
