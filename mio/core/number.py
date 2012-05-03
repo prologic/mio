012ced1b9a393eab0030bac6eddfb98ddf2d0a82
prologic <none@none>
2012-05-03 23:43:15 +1000

    Implemented better Range and Number

--- mio/core/number.py
+++ mio/core/number.py
@@ -4,11 +4,29 @@ from mio.pymethod import pymethod
 
 class Number(Object):
 
+    def __add__(self, other):
+        return self.value.__add__(other.value)
+
+    def __sub__(self, other):
+        return self.value.__sub__(other.value)
+
+    def __mul__(self, other):
+        return self.value.__mul__(other.value)
+
+    def __div__(self, other):
+        return self.value.__div__(other.value)
+
+    def __mod__(self, other):
+        return self.value.__mod__(other.value)
+
+    def __pow__(self, other):
+        return self.value.__pow__(other.value)
+
     def __int__(self):
-        return int(self.value)
+        return self.value.__int__()
 
     def __float__(self):
-        return float(self.value)
+        return self.value.__float__()
 
     def __repr__(self):
         return str(self.value)
@@ -19,34 +37,32 @@ class Number(Object):
 
     @pymethod()
     def add(self, other):
-        if type(self.value) is not type(other.value):
-            raise TypeError(self.add, self._type(), other._type())
-        return self.clone(self.value + other.value)
+        return self.clone(self + other)
 
     @pymethod()
     def sub(self, other):
-        return self.clone(self.value - other.value)
+        return self.clone(self - other)
 
     @pymethod()
     def mul(self, other):
-        return self.clone(self.value * other.value)
+        return self.clone(self * other)
 
     @pymethod()
     def div(self, other):
-        return self.clone(self.value / other.value)
+        return self.clone(self / other)
 
     @pymethod()
     def pow(self, other):
-        return self.clone(self.value ** other.value)
+        return self.clone(self ** other)
 
     @pymethod()
     def mod(self, other):
-        return self.clone(self.value % other.value)
+        return self.clone(self % other)
 
     @pymethod()
     def lshift(self, other):
-        return self.clone(self.value * 2 ** other.value)
+        return self.clone(self * 2 ** other)
 
     @pymethod()
     def rshift(self, other):
-        return self.clone(self.value / 2 ** other.value)
+        return self.clone(self/ 2 ** other)
--- mio/core/range.py
+++ mio/core/range.py
@@ -5,11 +5,17 @@ from mio.pymethod import pymethod
 
 class Range(Object):
 
-    def __iter__(self):
-        return self
+    def __init__(self, value=Null, parent=None):
+        super(Range, self).__init__(value=value, parent=parent)
 
-    def next(self):
-        return self.value.next()
+        self["start"] = self["Number"].clone(0)
+        self["stop"] = self["Number"].clone(0)
+        self["step"] = self["Number"].clone(1)
+
+    def __iter__(self):
+        while self["start"] < self["stop"]:
+            yield self["start"]
+            self["start"] = self["Number"].clone(self["start"] + self["step"])
 
     def __repr__(self):
         if self.value is not Null:
@@ -20,4 +26,22 @@ class Range(Object):
 
     @pymethod()
     def init(self, *args):
-        self.value = range(*args)
+        if len(args) == 3:
+            self["start"] = args[0]
+            self["stop"] = args[1]
+            self["step"] = args[2]
+        elif len(args) == 2:
+            self["start"] = args[0]
+            self["stop"] = args[1]
+            self["step"] = self["Number"].clone(1)
+        elif len(args) == 1:
+            self["start"] = self["Number"].clone(0)
+            self["stop"] = args[0]
+            self["step"] = self["Number"].clone(1)
+        else:
+            self["start"] = self["Number"].clone(0)
+            self["stop"] = self["Number"].clone(0)
+            self["step"] = self["Number"].clone(1)
+
+        keys = ("start", "stop", "step")
+        self.value = range(*[int(self[key]) for key in keys])
