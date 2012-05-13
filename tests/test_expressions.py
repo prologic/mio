2f21c88ae96c371aa039de8a8d909e95ef2e86bc
prologic <none@none>
2012-05-13 21:24:32 +1000

    Simplified assertion tests

--- tests/test_expressions.py
+++ tests/test_expressions.py
@@ -1,41 +1,33 @@
 def test_simple(mio):
-    v = mio.eval("1 + 2")
-    assert v == 3
+    assert mio.eval("1 + 2") == 3
 
 
 def test_complex(mio):
-    v = mio.eval("1 + 2 * 3")
-    assert v == 9
+    assert mio.eval("1 + 2 * 3") == 9
 
 
 def test_grouping(mio):
-    v = mio.eval("1 + (2 * 3)")
-    assert v == 7
+    assert mio.eval("1 + (2 * 3)") == 7
 
 
 def test_assignment(mio):
     mio.eval("x = 1")
-    v = mio.eval("x")
-    assert v == 1
+    assert mio.eval("x") == 1
 
 
 def test_complex_assignment1(mio):
     mio.eval("x = 1")
-    v = mio.eval("x")
-    assert v == 1
+    assert mio.eval("x") == 1
 
     mio.eval("x = x + 1")
-    v = mio.eval("x")
-    assert v == 2
+    assert mio.eval("x") == 2
 
 
 def test_complex_assignment2(mio):
     mio.eval("Foo = Object clone")
 
     mio.eval("Foo x = 1")
-    v = mio.eval("Foo x")
-    assert v == 1
+    assert mio.eval("Foo x") == 1
 
     mio.eval("Foo x = Foo x + 1")
-    v = mio.eval("x")
-    assert v == 2
+    assert mio.eval("x") == 2
