c891753251c22cef48232b7acfc3118d82a56ae2
prologic <none@none>
2012-05-14 22:51:37 +1000

    Don't use Decimal's __...__ special methods for arithmetic as it causes strange behavior

--- mio/core/number.py
+++ mio/core/number.py
@@ -15,28 +15,28 @@ class Number(Object):
         self["parent"] = runtime.state.find("Object")
 
     def __add__(self, other):
-        return self.value.__add__(other.value)
+        return self.value + other.value
 
     def __sub__(self, other):
-        return self.value.__sub__(other.value)
+        return self.value - other.value
 
     def __mul__(self, other):
-        return self.value.__mul__(other.value)
+        return self.value * other.value
 
     def __div__(self, other):
-        return self.value.__div__(other.value)
+        return self.value / other.value
 
     def __mod__(self, other):
-        return self.value.__mod__(other.value)
+        return self.value % other.value
 
     def __pow__(self, other):
-        return self.value.__pow__(other.value)
+        return self.value ** other.value
 
     def __int__(self):
-        return self.value.__int__()
+        return int(self.value)
 
     def __float__(self):
-        return self.value.__float__()
+        return float(self.value)
 
     def __str__(self):
         return str(self.value)
