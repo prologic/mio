dba8716907c3f386afe5887a41368a90911af495
prologic <none@none>
2012-05-20 01:41:39 +1000

    Fixed setting of type in clone

--- mio/object.py
+++ mio/object.py
@@ -268,8 +268,8 @@ class Object(object):
 
     @pymethod("clone")
     def _clone(self, receiver, context, m, *args):
-        if m.previous is not None:
-            type = runtime.find("String").clone(m.previous.args[0].name)
+        if m.first.previous is not None:
+            type = runtime.find("String").clone(m.first.previous.args[0].name)
         else:
             type = None
 
