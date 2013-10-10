f6b83f0cf3096615936b770c816ad5425377bb96
James Mills <j.mills@griffith.edu.au>
2013-10-10 10:18:18 +1000

    Code cleanup

--- mio/__main__.py
+++ mio/__main__.py
@@ -12,18 +12,25 @@ VERSION = "%prog v" + mio.__version__
 
 def parse_options():
     parser = OptionParser(usage=USAGE, version=VERSION)
+    add_option = parser.add_option
 
-    parser.add_option("-e", "",
-            action="store", default=None, dest="eval", metavar="expr",
-            help="evalulate the given expression and exit")
+    add_option(
+        "-e", "",
+        action="store", default=None, dest="eval", metavar="expr",
+        help="evalulate the given expression and exit"
+    )
 
-    parser.add_option("-i", "",
-            action="store_true", default=False, dest="interactive",
-            help="run interactively after processing the given files")
+    add_option(
+        "-i", "",
+        action="store_true", default=False, dest="interactive",
+        help="run interactively after processing the given files"
+    )
 
-    parser.add_option("-d", "",
-            action="store_true", default=False, dest="debug",
-            help="enable debugging output")
+    add_option(
+        "-d", "",
+        action="store_true", default=False, dest="debug",
+        help="enable debugging output"
+    )
 
     opts, args = parser.parse_args()
 
