02c14b0f6f70db6bcc4839c77554498deb2aa37b
James Mills <prologic@shortcircuit.net.au>
2013-10-13 01:12:25 +1000

    Code cleanup

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,16 +8,14 @@ TEST_FILE = os.path.join(os.path.dirname(__file__), "test.mio")
 
 
 def test_eval():
-    p = Popen([sys.executable, main_wrapper.__file__, "-e", "(1 + 2) print"],
-            stdout=PIPE)
+    p = Popen([sys.executable, main_wrapper.__file__, "-e", "(1 + 2) print"], stdout=PIPE)
     stdout = p.communicate()[0]
     assert p.returncode == 0
     assert stdout == "3"
 
 
 def test_interactive():
-    p = Popen([sys.executable, main_wrapper.__file__, "-i", TEST_FILE],
-            stdin=PIPE, stdout=PIPE)
+    p = Popen([sys.executable, main_wrapper.__file__, "-i", TEST_FILE], stdin=PIPE, stdout=PIPE)
     stdout = p.communicate("exit\n")[0]
     assert p.returncode == 0
     assert stdout.split()[0] == "3"
