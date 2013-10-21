9dfca733cd15f23055fdf4e4a92750f6ddaef56b
James Mills <j.mills@griffith.edu.au>
2013-10-21 16:25:06 +1000

    Added basic unit test for traits

---
+++ tests/test_traits.py
@@ -0,0 +1,31 @@
+def test_basic_trait(mio, capsys):
+    mio.eval("""
+        TGreeting = Object clone do (
+            hello = method(
+                writeln("Hello ", self getGreeting)
+            )
+        )
+
+        World = Object clone do (
+            uses(TGreeting)
+
+            greeting = "World!"
+
+            getGreeting = method(
+               return (self greeting)
+            )
+
+            setGreeting = method(aGreeting,
+                self greeting = aGreeting
+            )
+        )
+    """)
+
+    mio.eval("World hello")
+    out, err = capsys.readouterr()
+    assert out == "Hello World!\n"
+
+    mio.eval("World setGreeting(\"John\")")
+    mio.eval("World hello")
+    out, err = capsys.readouterr()
+    assert out == "Hello John!\n"
