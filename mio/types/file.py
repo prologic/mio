bf5df2d92622c519c8b866a44e34e14d49ec34f7
James Mills <prologic@shortcircuit.net.au>
2013-10-23 00:20:47 +1000

    Fixed File's constructor

--- mio/types/file.py
+++ mio/types/file.py
@@ -6,7 +6,10 @@ from mio.utils import method, Null
 class File(Object):
 
     def __init__(self, value=Null):
-        super(File, self).__init__(value=Null)
+        super(File, self).__init__(value=value)
+
+        if isinstance(value, file):
+            self.update_status()
 
         self.create_methods()
         self.parent = runtime.state.find("Object")
