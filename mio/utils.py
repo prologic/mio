46a86f408ff1028394b2cbb7e893a7c37d0bd8c0
James Mills <prologic@shortcircuit.net.au>
2013-10-27 13:01:01 +1000

    Borrowed ccav's @memoize decorator

--- mio/utils.py
+++ mio/utils.py
@@ -1,4 +1,8 @@
+from time import time
+from hashlib import md5
 from warnings import warn
+from threading import RLock
+from functools import wraps
 from inspect import getargspec, ismethod
 
 
@@ -57,6 +61,62 @@ def tryimport(modules, message=None):
 
     if message is not None:
         warn(message)
+
+
+class memoize(object):
+    """Memoize the results of a function.
+
+    Supports an optional timeout for automatic cache expiration.
+
+    If the optional manual_flush argument is True, a function called
+    "flush_cache" will be added to the wrapped function.  When
+    called, it will remove all the timed out values from the cache.
+
+    This decorator is thread safe.
+    This decorator also supports class and instance methods.
+
+    ..note:: Borrowed directly from the Wraptor library (https://pypi.python.org/pypi/Wraptor)
+             With modifications inspired from https://wiki.python.org/moin/PythonDecoratorLibrary#Memoize
+    """
+
+    def __init__(self, timeout=None, manual_flush=False):
+        self.timeout = timeout
+        self.manual_flush = manual_flush
+
+        self.cache = {}
+        self.cache_lock = RLock()
+
+    def __call__(self, fn):
+        def flush_cache():
+            with self.cache_lock:
+                for key in self.cache.keys():
+                    if (time() - self.cache[key][1]) > self.timeout:
+                        del(self.cache[key])
+
+        @wraps(fn)
+        def wrapped(*args, **kwargs):
+            kw = kwargs.items()
+            kw.sort()
+            key_str = repr((args, kw))
+            key = md5(key_str).hexdigest()
+
+            with self.cache_lock:
+                try:
+                    result, cache_time = self.cache[key]
+                    if self.timeout is not None and (time() - cache_time) > self.timeout:
+                        raise KeyError
+                except KeyError:
+                    result, _ = self.cache[key] = (fn(*args, **kwargs), time())
+
+            if not self.manual_flush and self.timeout is not None:
+                flush_cache()
+
+            return result
+
+        if self.manual_flush:
+            wrapped.flush_cache = flush_cache
+
+        return wrapped
 
 
 class MetaNull(type):
