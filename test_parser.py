31708ba801a432bf2ceee28a20245901a653e48f
James Mills <prologic@shortcircuit.net.au>
2013-10-31 00:04:40 +1000

    WHen using the test_parser don't pull in the site

--- test_parser.py
+++ test_parser.py
@@ -4,7 +4,15 @@ from mio import parser
 from mio import runtime
 
 
-runtime.init()
+class FakeOpts(object):
+
+    def __init__(self, **kwargs):
+        super(FakeOpts, self).__init__()
+
+        self.__dict__.update(**kwargs)
+
+
+runtime.init(opts=FakeOpts(nosys=True))
 eval = runtime.state.eval
 
 
