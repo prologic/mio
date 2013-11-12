c5bf424d3ce706473a2d3f92052842696ba1b101
James Mills <j.mills@griffith.edu.au>
2013-11-12 12:55:32 +1000

    Added help task

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -10,8 +10,9 @@ from __future__ import print_function
 from os import getcwd, path
 
 
-from fabric.api import abort, cd, execute, hide, hosts, lcd, local, prefix, prompt, run, settings, shell_env, task
+from fabric.tasks import Task
 from fabric.contrib.files import exists
+from fabric.api import abort, cd, execute, hide, hosts, lcd, local, prefix, prompt, run, settings, shell_env, task
 
 
 from .utils import msg, pip, requires, resolvepath, tobool
@@ -19,6 +20,39 @@ from .utils import msg, pip, requires, resolvepath, tobool
 
 # Path to pypy
 PYPY = resolvepath("$HOME/work/pypy")
+
+
+@task()
+def help(name=None):
+    """Display help for a given task
+
+    Options:
+        name    - The task to display help on.
+
+    To display a list of available tasks type:
+
+        $ fab -l
+
+    To display help on a specific task type:
+        
+        $ fab help:<name>
+    """
+
+    if name is None:
+        name = "help"
+
+    o = globals().get(name, None)
+    if isinstance(o, Task):
+        doc = getattr(o, "__doc__", None)
+        if doc is not None:
+            print("Help on {0:s}:".format(name))
+            print()
+            print(doc)
+        else:
+            print("No help available for {0;s}".format(name))
+    else:
+        print("No such task {0:s}".format(name))
+        print("For a list of tasks tpye: fab -l")
 
 
 @task()
