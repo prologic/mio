5f21455f57d599a1bf758f98bce13670231515ca
James Mills <prologic@shortcircuit.net.au>
2013-11-28 06:55:02 +1000

    Preparing 0.1.6 Development

--- mio/version.py
+++ mio/version.py
@@ -7,5 +7,5 @@
 So we only have to maintain version information in one place!
 """
 
-version_info = (0, 1, 5)  # (major, minor, patch, dev?)
+version_info = (0, 1, 6, "dev")  # (major, minor, patch, dev?)
 version = ".".join(map(str, version_info))
