ddefe89440eecd13c647821348529e80e807acba
James Mills <prologic@shortcircuit.net.au>
2013-12-03 19:33:58 +1000

    Sample RPython program that implements raw_input()

--- targets/target_test.py
+++ targets/target_test.py
@@ -5,10 +5,43 @@
 """RPython Entry Point: test"""
 
 
+try:
+    from rpython.rlib.streamio import open_file_as_stream
+except ImportError:
+    open_file_as_stream = open
+
+
+def input(prompt=None):
+    search = "\n"
+    stdin = open_file_as_stream("/dev/stdin", "r", 1024)
+    stdout = open_file_as_stream("/dev/stdout", "w", 1024)
+    if prompt is not None:
+        stdout.write(prompt)
+        stdout.flush()
+    while True:
+        try:
+            line = stdin.readline()
+        except (KeyboardInterrupt, EOFError):
+            return
+
+        if not line:
+            return
+        if search in line:
+            return line
+
+
 def main(argv):
     print("Hello World!")
+    name = input("What is your name? ")
+    if name is not None:
+        print("Hello " + name)
     return 0
 
 
 def target(*args):
     return main, None
+
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv)
