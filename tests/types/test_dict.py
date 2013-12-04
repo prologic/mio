aa8ae8ceea5b7d0cb3c441b29e77490d26ceaea4
James Mills <prologic@shortcircuit.net.au>
2013-12-04 17:46:42 +1000

    Fixed dict unit tests

--- tests/types/test_dict.py
+++ tests/types/test_dict.py
@@ -10,7 +10,7 @@ def test_clone(mio):
 
 
 def test_clone_dict(mio):
-    assert mio.eval("Dict clone(Dict clone() __setitem__(\"a\", 1))") == {"a": 1}
+    assert mio.frommio(mio.eval("Dict clone(Dict clone() __setitem__(\"a\", 1))")) == {u"a": 1}
 
 
 def test_repr(mio):
@@ -31,9 +31,9 @@ def test_setitem(mio):
     mio.eval("d __setitem__(\"a\", 1)")
     mio.eval("d __setitem__(\"b\", 2)")
     mio.eval("d __setitem__(\"c\", 3)")
-    assert mio.eval("d") == {"a": 1, "b": 2, "c": 3}
+    assert mio.frommio(mio.eval("d")) == {u"a": 1, u"b": 2, u"c": 3}
 
-    assert mio.eval("d __setitem__(\"d\", 4)") == {"a": 1, "b": 2, "c": 3, "d": 4}
+    assert mio.frommio(mio.eval("d __setitem__(\"d\", 4)")) == {u"a": 1, u"b": 2, u"c": 3, u"d": 4}
 
 
 def test_getitem(mio):
@@ -41,7 +41,7 @@ def test_getitem(mio):
     mio.eval("d __setitem__(\"a\", 1)")
     mio.eval("d __setitem__(\"b\", 2)")
     mio.eval("d __setitem__(\"c\", 3)")
-    assert mio.frommio(mio.eval("d")) == {"a": 1, "b": 2, "c": 3}
+    assert mio.frommio(mio.eval("d")) == {u"a": 1, u"b": 2, u"c": 3}
 
     assert mio.eval("d __getitem__(\"a\")") == 1
 
@@ -51,7 +51,7 @@ def test_len(mio):
     mio.eval("d __setitem__(\"a\", 1)")
     mio.eval("d __setitem__(\"b\", 2)")
     mio.eval("d __setitem__(\"c\", 3)")
-    assert mio.eval("d") == {"a": 1, "b": 2, "c": 3}
+    assert mio.frommio(mio.eval("d")) == {u"a": 1, u"b": 2, u"c": 3}
 
     assert mio.eval("d len") == 3
 
@@ -61,9 +61,9 @@ def test_keys(mio):
     mio.eval("d __setitem__(\"a\", 1)")
     mio.eval("d __setitem__(\"b\", 2)")
     mio.eval("d __setitem__(\"c\", 3)")
-    assert mio.eval("d") == {"a": 1, "b": 2, "c": 3}
+    assert mio.frommio(mio.eval("d")) == {u"a": 1, u"b": 2, u"c": 3}
 
-    assert sorted(mio.eval("d keys")) == ["a", "b", "c"]
+    assert sorted(mio.eval("d keys")) == [u"a", u"b", u"c"]
 
 
 def test_values(mio):
@@ -71,6 +71,6 @@ def test_values(mio):
     mio.eval("d __setitem__(\"a\", 1)")
     mio.eval("d __setitem__(\"b\", 2)")
     mio.eval("d __setitem__(\"c\", 3)")
-    assert mio.eval("d") == {"a": 1, "b": 2, "c": 3}
+    assert mio.frommio(mio.eval("d")) == {u"a": 1, u"b": 2, u"c": 3}
 
     assert sorted(mio.eval("d values")) == [1, 2, 3]
