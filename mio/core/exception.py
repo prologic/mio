6e8b79ad686da4213a1c6ebf96558e9000fc3551
James Mills <prologic@shortcircuit.net.au>
2013-12-04 23:11:17 +1000

    Test Coverage

--- mio/core/exception.py
+++ mio/core/exception.py
@@ -9,9 +9,6 @@ from mio.errors import UserError
 
 class Exception(Object):
 
-    def __repr__(self):
-        return "{0:s}({1:s})".format(self.type, self.value or "")
-
     @method("try")
     def tryEval(self, receiver, context, m, code):
         try:
