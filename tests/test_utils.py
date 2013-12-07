411fe23aeaffdc1b7e5dda8f76964f7c1ce58322
James Mills <prologic@shortcircuit.net.au>
2013-12-07 22:11:47 +1000

    Test Coverage

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,15 @@ import sys
 
 
 from mio.object import Object
-from mio.utils import format_method, format_object, method, tryimport, Null
+from mio.utils import default_repr, format_function, format_method, format_object, format_result, format_value, method, tryimport, Null
+
+
+def foo():
+    pass
+
+
+def bar():
+    "Return \"Bar\""
 
 
 class Foo(Object):
@@ -36,6 +44,67 @@ def test_format_method(mio):
     assert format_method(foo.noargs) == "noargs()"
     assert format_method(foo.args) == "args(a, b, c)"
     assert format_method(foo.varargs) == "varargs(*args)"
+
+
+def test_format_function(mio):
+    assert format_function(foo) == "foo()"
+    assert format_function(bar) == "Return \"Bar\""
+
+
+def test_format_value():
+    obj = Foo()
+    assert format_value(obj.noargs) == "noargs()"
+    assert format_value(obj.args) == "args(a, b, c)"
+    assert format_value(obj.varargs) == "varargs(*args)"
+
+
+def test_format_value2():
+    assert format_value(foo) == "foo()"
+    assert format_value(bar) == "Return \"Bar\""
+
+
+def test_format_value3():
+    assert format_value(1) == "1"
+
+
+def test_format_result():
+    obj = Foo()
+    assert format_result(obj.noargs) == "noargs()"
+    assert format_result(obj.args) == "args(a, b, c)"
+    assert format_result(obj.varargs) == "varargs(*args)"
+
+
+def test_format_result2():
+    assert format_result(foo) == "foo()"
+    assert format_result(bar) == "Return \"Bar\""
+
+
+def test_format_result3():
+    assert format_result(1) == "1"
+
+
+def test_format_result4(mio):
+    assert format_result(mio.eval("None")) is None
+
+
+def test_format_result5(mio):
+    mio.eval("""
+        Foo = Object clone() do(
+            __repr__ = method("Foo")
+        )
+    """)
+
+    assert format_result(mio.eval("Foo")) == "Foo"
+
+
+def test_format_result6(mio):
+    Foo = mio.eval("Foo = Object clone()")
+    assert format_result(mio.eval("Foo")) == "Foo(Object) at {0:s}".format(hex(id(Foo)))
+
+
+def test_default_repr(mio):
+    Foo = mio.eval("Foo = Object clone()")
+    assert default_repr(mio.eval("Foo")) == "Foo(Object) at {0:s}".format(hex(id(Foo)))
 
 
 def test_tryimport(mio):
